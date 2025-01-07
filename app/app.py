0b6710808bbd4b3e81b51d34a1a98a822e436260
Abigailnkole96 <Abigailnkole96@gmail.com>
2025-01-07 20:10:50 +0200

    clean up

--- app/app.py
+++ app/app.py
@@ -21,43 +21,15 @@ def predict():
     output = round(model.predict(input_data)[0][0])
     if output == 0:
         decision = "Rejected"
-        reason = "---"
     else:
         decision = "Approved"
-        reason = "---"
-
-    # Calculate Debt-to-Income (DTI) ratio
-    #annual_salary = salary * 12  # Convert monthly salary to annual
-    #dti_ratio = (loan_amount / annual_salary) * 100
-#
-    ## Approval logic
-    #if salary >= 2500 and credit_score >= 700 and dti_ratio < 35:
-    #    decision = "Approved"
-    #    reason = "Your salary, credit score, and DTI ratio meet the approval criteria."
-    #elif salary < 2000:
-    #    decision = "Rejected"
-    #    reason = f"Your salary (€{salary}) is below the required minimum of €2,000 per month."
-    #elif credit_score < 650:
-    #    decision = "Rejected"
-    #    reason = f"Your credit score of {credit_score} is below the minimum required score of 650."
-    #elif dti_ratio > 40:
-    #    decision = "Rejected"
-    #    reason = f"Your debt-to-income (DTI) ratio of {dti_ratio:.2f}% exceeds the allowable limit of 40%."
-    #elif dti_ratio >= 35:
-    #    decision = "Further review needed"
-    #    reason = f"Your DTI ratio of {dti_ratio:.2f}% is between 35% and 40%, so further review is required."
-    #else:
-    #    decision = "Further review needed"
-    #    reason = "Some criteria do not meet the requirements, and further review is required."
 
     # Prepare the response to be sent back to the web app
     return render_template('result.html', 
                             decision=decision, 
-                            reason=reason, 
                             salary=salary, 
                             credit_score=credit_score, 
-                            loan_amount=loan_amount, 
-                            dti_ratio=1.0)
+                            loan_amount=loan_amount)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
--- app/tests/test_example.py
+++ app/tests/test_example.py
@@ -1,4 +1,4 @@
-# app/tests/test_example.py
+# app/tests/test_example.py  ##ensure that what is bing addedon the first page is what shows up in the second page
 
 def test_sample():
     assert 1 + 1 == 2
